8e2b5668e2a92532a04f2cd4ebb9b554e731aa0e
JonAJ <jonajmail@gmail.com>
2024-11-19 14:34:38 +0300

    domain tests

--- AuthorizationService/app/domain/entities/user.py
+++ AuthorizationService/app/domain/entities/user.py
@@ -1,19 +1,28 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from uuid import uuid4
 
-from domain.entities.base import BaseEntity
+#from domain.entities.base import BaseEntity
 from domain.values.email import Email
 from domain.values.password import Password
 from domain.values.login import Login
 from domain.values.role import Role
 
 @dataclass
-class User(BaseEntity):
+class User:
+    oid: str = field(
+        default_factory=lambda: str(uuid4()),
+        kw_only=True
+    )
     login: Login
     password: Password
     email: Email
     role: Role
     
+    def __hash__(self) -> int:
+        return hash(self.oid)
     
+    def __eq__(self, __value: 'User') -> bool:
+        return self.oid == __value.oid
     
     
     
